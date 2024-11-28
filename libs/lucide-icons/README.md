e394fa708a83d6af23edc36802baf518332d35a4
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-11-28 14:44:38 +0100

    work in progress

---
+++ libs/lucide-icons/README.md
@@ -0,0 +1,48 @@
+# lucide-icons
+
+# @semantic-icons/tabler-icons
+
+`@semantic-icons/tabler-icons` helps developers to use [Tabler icons](https://tabler.io/icons) inside [Angular](https://angular.dev) projects.
+
+## Supported versions
+
+| @semantic-icons/tabler-icons | Angular   | Tabler-icons |
+| ---------------------------- | --------- | ------------ |
+| 0.x.x                        | >= 17.1.0 | 3.22.0       |
+
+## Usage
+
+First, install `@semantic-icons/tabler-icons` from npm:
+
+```sh
+npm install @semantic-icons/tabler-icons
+```
+
+Now each icon can be imported individually as an Angular component:
+
+```js
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+import { SvgCakeIcon } from '@semantic-icons/tabler-icons/outline';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [SvgCakeIcon],
+  template: `
+    <svg-cake-icon class="size-6 text-blue-500" />
+  `,
+  styles: ``,
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomePageComponent {}
+```
+
+The outline icons can be imported from `@semantic-icons/tabler-icons/outline`, the filled icons can be imported from `@semantic-icons/tabler-icons/filled`.
+
+Icons use the Angular naming convention and are always prefixed with the word `svg` and suffixed with the word `icon`.
+
+## License
+
+MIT © 2024-2024 Khalil LAGRIDA
---
+++ libs/nx-generators/src/generators/lucide-icons/generator.ts
@@ -0,0 +1,57 @@
+import { Tree, formatFiles, generateFiles, names } from '@nx/devkit';
+import * as path from 'path';
+
+import { LucideIconsGeneratorSchema } from './schema';
+
+export async function lucideIconsGenerator(
+  tree: Tree,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  options: LucideIconsGeneratorSchema,
+) {
+  const iconsLibPath = 'libs/lucide-icons';
+
+  const iconsSourcePath = 'node_modules/lucide-static/icons';
+  const iconsDestinationPath = path.join(iconsLibPath, 'src');
+
+  generateIconsComponents(tree, iconsSourcePath, iconsDestinationPath);
+
+  await formatFiles(tree);
+}
+
+function generateIconsComponents(
+  tree: Tree,
+  iconsSourcePath: string,
+  iconsDestinationPath: string,
+) {
+  const exports = [];
+  tree.children(iconsSourcePath).forEach((fileName) => {
+    const name = path.parse(fileName).name;
+
+    const svgContent1 = tree.read(
+      path.join(iconsSourcePath, fileName),
+      'utf-8',
+    );
+
+    const re = /(<svg)/;
+    const svgContent = svgContent1.replace(re, '$1 [ngClass]="class()"');
+
+    const svgClassName = `Svg${names(name).className}Icon`;
+    const svgFileName = `svg-${names(name).fileName}-icon`;
+    const svgSelector = `svg-${names(name).fileName}-icon`;
+
+    exports.push(`export * from './icons/${svgFileName}';`);
+
+    const o = { svgContent, svgClassName, svgFileName, svgSelector };
+
+    generateFiles(
+      tree,
+      path.join(__dirname, '..', 'heroicons', 'files', 'component'),
+      path.join(iconsDestinationPath, 'icons'),
+      o,
+    );
+  });
+
+  tree.write(path.join(iconsDestinationPath, 'index.ts'), exports.join('\r\n'));
+}
+
+export default lucideIconsGenerator;
