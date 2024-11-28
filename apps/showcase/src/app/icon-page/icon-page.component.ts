2daf3be3b5e83979497c7caf3a359851beb93015
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-11-28 13:20:32 +0100

    work in progress

--- apps/showcase/src/app/icon-page/icon-page.component.ts
+++ apps/showcase/src/app/icon-page/icon-page.component.ts
@@ -4,16 +4,16 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 
-import { SvgMaIcon } from '@semantic-icons/flag-icons/square';
+import { SvgMaFlagIcon } from '@semantic-icons/flag-icons/square';
 import { SvgBellIcon } from '@semantic-icons/flowbite-icons/solid';
 
 @Component({
   selector: 'app-icon-page',
   standalone: true,
-  imports: [SvgBellIcon, SvgMaIcon],
+  imports: [SvgBellIcon, SvgMaFlagIcon],
   template: `
     <svg-bell-icon class="size-6 text-blue-500" />
-    <svg-ma-icon class="size-36" />
+    <svg-ma-flag-icon class="size-36" />
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
