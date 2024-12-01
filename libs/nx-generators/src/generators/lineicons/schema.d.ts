5a022cd142f5fea0fafcf0ed33520ffc9044e208
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-01 11:57:59 +0100

    work in progress

--- libs/nx-generators/src/generators/lineicons/schema.d.ts
+++ libs/nx-generators/src/generators/lineicons/schema.d.ts
@@ -1,3 +1,2 @@
-export interface LineiconsGeneratorSchema {
-  name: string;
-}
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-empty-interface
+export interface LineiconsGeneratorSchema {}
