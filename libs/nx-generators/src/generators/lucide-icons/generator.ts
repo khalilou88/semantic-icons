ce9ae158e7cb0f9e189e0f213aef4308e7c869ee
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-01-02 15:40:15 +0100

    build: work in progress

--- libs/nx-generators/src/generators/lucide-icons/generator.ts
+++ libs/nx-generators/src/generators/lucide-icons/generator.ts
@@ -47,9 +47,18 @@ function generateIconsComponents(
 
     const regex = /<svg[^>]*>([\s\S]*)<\/svg>/;
     const matches = regex.exec(svgFileContent) ?? [];
-    const svgContent = matches[1]
-      .replace(/<path/g, '<svg:path')
-      .replace(/<rect/g, '<svg:rect');
+    let svgContent = matches.length > 1 ? matches[1] : '';
+
+    // Define the prefix
+    const prefix = 'svg:';
+
+    // Add prefix to all SVG tags (like <circle>, <rect>, <path>, etc.)
+    svgContent = svgContent.replace(
+      /<(\/?)(circle|rect|path|line|polygon|polyline|ellipse|text)([^>]*)>/gi,
+      (match, closing, tagName, attributes) => {
+        return `<${closing}${prefix}${tagName}${attributes}>`;
+      },
+    );
 
     const svgFileName = `${names(name).fileName}-icon`;
     const svgClassName = `Si${names(name).className}Icon`;
