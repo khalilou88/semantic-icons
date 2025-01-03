4ebaadbe63cef8a2dfa9994ee18a6ce9acf9e90f
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-01-03 01:38:11 +0100

    build: work in progress

--- apps/showcase/src/app/home-page.ts
+++ apps/showcase/src/app/home-page.ts
@@ -4,14 +4,14 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 
-import { SiFrFlagIcon } from '@semantic-icons/circle-flags';
+import { SiIt42FlagIcon } from '@semantic-icons/circle-flags';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [SiFrFlagIcon],
+  imports: [SiIt42FlagIcon],
   template: `
-    <svg class="size-36" si-fr-flag-icon></svg>
+    <svg class="size-36" si-it-42-flag-icon></svg>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
