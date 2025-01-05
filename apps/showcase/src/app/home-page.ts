285954c3256253b6851f2d4891bca9e69fabb15c
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-01-05 13:12:28 +0100

    build: work in progress

--- apps/showcase/src/app/home-page.ts
+++ apps/showcase/src/app/home-page.ts
@@ -10,9 +10,9 @@ import { SiGithubIcon, SiNpmIcon } from '@semantic-icons/simple-icons';
   selector: 'app-home-page',
   imports: [SiGithubIcon, SiNpmIcon],
   template: `
-    <svg class="size-36" fill="#181717" si-github-icon></svg>
+    <svg class="size-36" si-github-icon></svg>
 
-    <svg class="size-36" fill="#CB3837" si-npm-icon></svg>
+    <svg class="size-36" si-npm-icon></svg>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
