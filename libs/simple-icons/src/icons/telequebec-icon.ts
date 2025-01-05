796de3f48d9df9c2e9c142b7c4ca598fe9f4272f
khalilou88 <khalilou88@users.noreply.github.com>
2025-01-05 12:10:44 +0000

    build: generate icons

--- libs/simple-icons/src/icons/telequebec-icon.ts
+++ libs/simple-icons/src/icons/telequebec-icon.ts
@@ -31,5 +31,6 @@ export class SiTelequebecIcon implements OnInit {
     this.render.setAttribute(svg, 'role', 'img');
     this.render.setAttribute(svg, 'xmlns', 'http://www.w3.org/2000/svg');
     this.render.setAttribute(svg, 'viewBox', '0 0 24 24');
+    this.render.setAttribute(svg, 'fill', '1343FB');
   }
 }
