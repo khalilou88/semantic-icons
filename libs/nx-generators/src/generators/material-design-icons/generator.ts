75220252886f065b373251cadd4d18e6e9aad9da
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-01-07 14:14:00 +0100

    build: work in progress

--- libs/nx-generators/src/generators/material-design-icons/generator.ts
+++ libs/nx-generators/src/generators/material-design-icons/generator.ts
@@ -7,16 +7,44 @@ import {
 } from '@nx/devkit';
 import * as fs from 'fs';
 import * as path from 'path';
+import simpleGit from 'simple-git';
+import { dirSync } from 'tmp';
 
 import { getSvgAttributes, getSvgTagContent } from '../../utils';
-import { FlowbiteIconsGeneratorSchema } from './schema';
+import { MaterialDesignIconsGeneratorSchema } from './schema';
 
-export async function flowbiteIconsGenerator(
+const repoUrl = 'https://github.com/google/material-design-icons.git';
+
+// Create a temporary directory
+const tempDir: string = dirSync({
+  prefix: 'material-design-icons-',
+  unsafeCleanup: true,
+}).name;
+
+// Initialize simple-git
+const git = simpleGit();
+
+async function cloneRepo(): Promise<void> {
+  try {
+    console.log(
+      `Cloning repository from ${repoUrl} into temporary directory: ${tempDir}...`,
+    );
+    await git.clone(repoUrl, tempDir);
+    console.log('Repository cloned successfully!');
+    console.log(`Cloned files are available at: ${tempDir}`);
+  } catch (error) {
+    console.error('Failed to clone the repository:', (error as Error).message);
+  }
+}
+
+export async function materialDesignIconsGenerator(
   tree: Tree,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  options: FlowbiteIconsGeneratorSchema,
+  options: MaterialDesignIconsGeneratorSchema,
 ) {
-  const iconsDestinationPath = 'libs/flowbite-icons';
+  cloneRepo();
+
+  const iconsDestinationPath = 'libs/material-design-icons';
 
   //1
   const filledIconsSourcePath = 'node_modules/flowbite-icons/src/solid';
@@ -144,4 +172,4 @@ const a = function f(
   );
 };
 
-export default flowbiteIconsGenerator;
+export default materialDesignIconsGenerator;
