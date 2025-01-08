c5019cca8575c4660311e124370e134be03bb141
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-01-08 23:40:47 +0100

    feat: add class

--- libs/lucide-icons/src/icons/a-arrow-down-icon.ts
+++ libs/lucide-icons/src/icons/a-arrow-down-icon.ts
@@ -6,6 +6,7 @@ import {
   Renderer2,
   ViewEncapsulation,
   inject,
+  input,
 } from '@angular/core';
 
 @Component({
@@ -18,6 +19,9 @@ import {
     <svg:path d="M18 7v9" />
     <svg:path d="m14 12 4 4 4-4" />
   `,
+  host: {
+    '[class]': 'class()',
+  },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -25,6 +29,8 @@ import {
 export class SiAArrowDownIcon implements OnInit {
   private readonly elementRef = inject(ElementRef);
   private readonly render = inject(Renderer2);
+
+  readonly class = input<string>('');
 
   ngOnInit(): void {
     const svg = this.elementRef.nativeElement;
