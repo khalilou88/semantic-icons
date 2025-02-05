73dce8cae4e98122fc882578d96068aea8b5e37e
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-02-05 13:05:09 +0100

    build: work in progress

--- libs/nx-generators/src/executors/copy-license/executor.ts
+++ libs/nx-generators/src/executors/copy-license/executor.ts
@@ -1,4 +1,4 @@
-import { ExecutorContext, workspaceRoot } from '@nx/devkit';
+import { ExecutorContext, logger, workspaceRoot } from '@nx/devkit';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -8,6 +8,7 @@ export default async function runExecutor(
   options: CopyLicenseExecutorSchema,
   context: ExecutorContext,
 ): Promise<{ success: boolean }> {
+  logger.info('Copy License');
   const src = path.join(workspaceRoot, 'LICENSE');
 
   const dest = path.join(workspaceRoot, 'dist', 'libs', context.projectName);
@@ -15,7 +16,8 @@ export default async function runExecutor(
   let success = true;
   try {
     fs.copyFileSync(src, dest);
-  } catch {
+  } catch (err) {
+    logger.error(`Error: ${JSON.stringify(err)}`);
     success = false;
   }
 
