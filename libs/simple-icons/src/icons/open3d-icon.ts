02cf7d8e859c18f5ae43f25c515890a2b97e6a00
khalilou88 <khalilou88@users.noreply.github.com>
2025-02-05 02:47:33 +0000

    feat: generate icons

--- libs/simple-icons/src/icons/open3d-icon.ts
+++ libs/simple-icons/src/icons/open3d-icon.ts
@@ -7,13 +7,13 @@ import {
 } from '@angular/core';
 
 @Component({
-  selector: 'svg[si-expo-icon]',
+  selector: 'svg[si-open3d-icon]',
   standalone: true,
   imports: [],
   template: `
-    <svg:title>Expo</svg:title>
+    <svg:title>Open3D</svg:title>
     <svg:path
-      d="M0 20.084c.043.53.23 1.063.718 1.778.58.849 1.576 1.315 2.303.567.49-.505 5.794-9.776 8.35-13.29a.761.761 0 011.248 0c2.556 3.514 7.86 12.785 8.35 13.29.727.748 1.723.282 2.303-.567.57-.835.728-1.42.728-2.046 0-.426-8.26-15.798-9.092-17.078-.8-1.23-1.044-1.498-2.397-1.542h-1.032c-1.353.044-1.597.311-2.398 1.542C8.267 3.991.33 18.758 0 19.77Z"
+      d="M5.998 1.606 0 12l5.998 10.394h2.094l1.954-3.383H7.952L4.06 12.268h8.149l3.56 6.157L19.483 12l-3.715-6.444-3.56 6.18H4.055l3.893-6.747h2.098L8.088 1.606Zm2.71 0 1.954 3.383h5.386L20.096 12l-4.044 7.011h-5.394l-1.954 3.383h9.294l.488-.847L24 12 18.002 1.606Z"
     />
   `,
   host: {
@@ -26,10 +26,10 @@ import {
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SiExpoIcon {
+export class SiOpen3dIcon {
   protected readonly xmlns = signal<string>('http://www.w3.org/2000/svg');
 
   readonly viewBox = input<string>('0 0 24 24');
 
-  readonly fill = input<string>('#1C2024');
+  readonly fill = input<string>('#000000');
 }
