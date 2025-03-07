1666c0abe11a0906f8c14dbca743ac2462556a6a
khalilou88 <khalilou88@users.noreply.github.com>
2025-03-07 12:58:55 +0000

    feat: generate icons

--- libs/lucide-icons/src/icons/brackets-icon.ts
+++ libs/lucide-icons/src/icons/brackets-icon.ts
@@ -11,8 +11,8 @@ import {
   standalone: true,
   imports: [],
   template: `
-    <svg:path d="M16 3h3v18h-3" />
-    <svg:path d="M8 21H5V3h3" />
+    <svg:path d="M16 3h2a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1h-2" />
+    <svg:path d="M8 21H6a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h2" />
   `,
   host: {
     '[attr.xmlns]': 'xmlns()',
