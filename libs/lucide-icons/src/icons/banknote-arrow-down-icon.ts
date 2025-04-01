d190a663c8221d30e0402828bbbcd569152536cf
khalilou88 <khalilou88@users.noreply.github.com>
2025-04-01 03:14:51 +0000

    feat: generate icons

---
+++ libs/lucide-icons/src/icons/banknote-arrow-down-icon.ts
@@ -0,0 +1,61 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+  input,
+  signal,
+} from '@angular/core';
+
+@Component({
+  selector: 'svg[si-banknote-arrow-down-icon]',
+  standalone: true,
+  imports: [],
+  template: `
+    <svg:path d="M12 18H4a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5" />
+    <svg:path d="m16 19 3 3 3-3" />
+    <svg:path d="M18 12h.01" />
+    <svg:path d="M19 16v6" />
+    <svg:path d="M6 12h.01" />
+    <svg:circle cx="12" cy="12" r="2" />
+  `,
+  host: {
+    '[attr.xmlns]': 'xmlns()',
+    '[attr.width]': 'width()',
+    '[attr.height]': 'height()',
+    '[attr.viewBox]': 'viewBox()',
+    '[attr.fill]': 'fill()',
+    '[attr.stroke]': 'stroke()',
+    '[attr.stroke-width]': 'strokeWidth()',
+    '[attr.stroke-linecap]': 'strokeLinecap()',
+    '[attr.stroke-linejoin]': 'strokeLinejoin()',
+    'data-slot': 'icon',
+  },
+  styles: ``,
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SiBanknoteArrowDownIcon {
+  protected readonly xmlns = signal<string>('http://www.w3.org/2000/svg');
+
+  readonly width = input<string | number>('24');
+
+  readonly height = input<string | number>('24');
+
+  readonly viewBox = input<string>('0 0 24 24');
+
+  readonly fill = input<string>('none');
+
+  readonly stroke = input<string>('currentColor');
+
+  readonly strokeWidth = input<string | number>('2', {
+    alias: 'stroke-width',
+  });
+
+  readonly strokeLinecap = input<string>('round', {
+    alias: 'stroke-linecap',
+  });
+
+  readonly strokeLinejoin = input<string>('round', {
+    alias: 'stroke-linejoin',
+  });
+}
