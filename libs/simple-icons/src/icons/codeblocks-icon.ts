41ffe932f526563e1d4a276586bfcb3a96e09926
khalilou88 <khalilou88@users.noreply.github.com>
2025-05-30 13:16:32 +0000

    feat: generate icons

--- libs/simple-icons/src/icons/codeblocks-icon.ts
+++ libs/simple-icons/src/icons/codeblocks-icon.ts
@@ -6,7 +6,7 @@ import {
 } from '@angular/core';
 
 @Component({
-  selector: 'svg[si-code::blocks-icon]',
+  selector: 'svg[si-codeblocks-icon]',
   standalone: true,
   imports: [],
   template: `
@@ -25,7 +25,7 @@ import {
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SiCodeBlocksIcon {
+export class SiCodeblocksIcon {
   protected readonly xmlns = 'http://www.w3.org/2000/svg';
 
   readonly viewBox = input<string>('0 0 24 24');
--- libs/simple-icons/src/icons/microbit-icon.ts
+++ libs/simple-icons/src/icons/microbit-icon.ts
@@ -6,7 +6,7 @@ import {
 } from '@angular/core';
 
 @Component({
-  selector: 'svg[si-micro:bit-icon]',
+  selector: 'svg[si-microbit-icon]',
   standalone: true,
   imports: [],
   template: `
@@ -25,7 +25,7 @@ import {
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SiMicroBitIcon {
+export class SiMicrobitIcon {
   protected readonly xmlns = 'http://www.w3.org/2000/svg';
 
   readonly viewBox = input<string>('0 0 24 24');
