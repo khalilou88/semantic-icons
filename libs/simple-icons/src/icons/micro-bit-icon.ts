9b4d0ad89377a62798f7e98dc5f1d5db79a16906
khalilou88 <khalilou88@users.noreply.github.com>
2025-05-30 13:27:33 +0000

    feat: generate icons

--- libs/simple-icons/src/icons/micro-bit-icon.ts
+++ libs/simple-icons/src/icons/micro-bit-icon.ts
@@ -6,7 +6,7 @@ import {
 } from '@angular/core';
 
 @Component({
-  selector: 'svg[si-microbit-icon]',
+  selector: 'svg[si-micro-bit-icon]',
   standalone: true,
   imports: [],
   template: `
@@ -25,7 +25,7 @@ import {
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SiMicrobitIcon {
+export class SiMicroBitIcon {
   protected readonly xmlns = 'http://www.w3.org/2000/svg';
 
   readonly viewBox = input<string>('0 0 24 24');
