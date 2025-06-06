42c7f6301139f1b5afbfac2d92f368192f97e48f
khalilou88 <khalilou88@users.noreply.github.com>
2025-06-06 13:13:10 +0000

    feat: generate icons

--- libs/simple-icons/src/icons/ifix-it-icon.ts
+++ libs/simple-icons/src/icons/ifix-it-icon.ts
@@ -25,7 +25,7 @@ import {
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SiIfixItIcon {
+export class SiIFixItIcon {
   protected readonly xmlns = 'http://www.w3.org/2000/svg';
 
   readonly viewBox = input<string>('0 0 24 24');
