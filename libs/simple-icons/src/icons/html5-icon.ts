ca474de590822e0c6d1afee7e3cab26627b29d87
khalilou88 <khalilou88@users.noreply.github.com>
2025-06-20 10:25:17 +0000

    feat: generate icons

---
+++ libs/simple-icons/src/icons/html5-icon.ts
@@ -0,0 +1,34 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+  input,
+} from '@angular/core';
+
+@Component({
+  selector: 'svg[si-html5-icon]',
+  standalone: true,
+  imports: [],
+  template: `
+    <svg:title>HTML5</svg:title>
+    <svg:path
+      d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z"
+    />
+  `,
+  host: {
+    role: 'img',
+    '[attr.xmlns]': 'xmlns',
+    '[attr.viewBox]': 'viewBox()',
+    '[attr.fill]': 'fill()',
+  },
+  styles: ``,
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SiHTML5Icon {
+  protected readonly xmlns = 'http://www.w3.org/2000/svg';
+
+  readonly viewBox = input<string>('0 0 24 24');
+
+  readonly fill = input<string>('#E34F26');
+}
